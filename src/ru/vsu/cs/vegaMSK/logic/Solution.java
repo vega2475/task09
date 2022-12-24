427c772c69ed94a2c07ec9e25d59be0bebd9e949
nikmenshikov <kolya.menshikov.03@list.ru>
2022-12-24 13:43:56 +0300

    Version 1.3

--- src/ru/vsu/cs/vegaMSK/logic/Solution.java
+++ src/ru/vsu/cs/vegaMSK/logic/Solution.java
@@ -1,15 +1,15 @@
 package ru.vsu.cs.vegaMSK.logic;
+
 import java.util.ArrayList;
 import java.util.List;
 
 //4 пп
 public class Solution {
-    public static ArrayList <Integer> sol (ArrayList <Integer> list) {
+    public static ArrayList<Integer> sol(ArrayList<Integer> list) {
         if (list == null || list.size() == 0) {//проверка на пустоту листа
             System.out.println("empty array");
             return null;
         }
-
         int maxSumStartIndex = 0;
         int maxSumLastIndex = 0;
         int maxSum = list.get(0);
@@ -21,7 +21,7 @@ public class Solution {
 
             lastSum += list.get(i);
 
-            if(lastSum == list.get(i)){//доп условие
+            if (lastSum == list.get(i)) {//доп условие
                 lastSumStartIndex = i;
             }
 
@@ -30,35 +30,20 @@ public class Solution {
                 lastSumStartIndex = i;
             }
 
-            if (maxSum < lastSum ) {
+            if (maxSum < lastSum) {
                 maxSumStartIndex = lastSumStartIndex;
                 maxSumLastIndex = i;
                 maxSum = lastSum;
             }
 
-            if(lastSum == maxSum){
-                if (i - lastSumStartIndex < maxSumLastIndex - maxSumStartIndex){
+            if (lastSum == maxSum) {
+                if (i - lastSumStartIndex < maxSumLastIndex - maxSumStartIndex) {
                     maxSumStartIndex = lastSumStartIndex;
                     maxSumLastIndex = i;
                 }
             }
-//            if (maxSum == lastSum) {
-//                if (maxSumLastIndex - maxSumStartIndex < i - lastSumStartIndex) continue;//для минимальной длины
-//                maxSumStartIndex = lastSumStartIndex;//крч надо чтобы тут 11
-//                maxSumLastIndex = i;// а тут 12
-//                maxSumLength = maxSumLastIndex - maxSumStartIndex + 1;
-//            }
-//            if(list.get(maxSumStartIndex) + list.get(maxSumLastIndex) == maxSum && maxSumStartIndex > 0 && !Objects.equals(list.get(maxSumStartIndex), list.get(maxSumLastIndex))){
-//                maxSumStartIndex = list.indexOf(maxSum - list.get(maxSumStartIndex));
-//                maxSumLastIndex = list.indexOf(maxSum -  list.get(maxSumLastIndex));
-//            }
-//            if(maxSumStartIndex > 4 && maxSumLastIndex > 5){
-//                maxSumStartIndex = 4;
-//                maxSumLastIndex = 5;
-//            }
 
         }
-
         System.out.println("sum( arr[" + maxSumStartIndex + "] .. arr[" + maxSumLastIndex + "] ) = " + maxSum);
         ArrayList<Integer> listCopy = new ArrayList<>();
         for (int i = maxSumStartIndex; i <= maxSumLastIndex; i++) {
@@ -67,6 +52,4 @@ public class Solution {
         }
         return listCopy;
     }
-
-
 }
